7a1d25436d7b0b754be19b1c9b707ebfd5311f97
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 10:19:44 +0700

    fix

---
+++ src/if1/pkg10119006/latihan13/IF110119006Latihan13.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan13;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil Relational
+ */
+public class IF110119006Latihan13 {
+
+    public static void main(String[] args) {
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b = " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("b >= a = " + (bil2 >= bill));
+        System.out.println("b <= a = " + (bil2 <= bill));
+    }
+    
+}
